adfc24c156c49e5b081cc44c59791bf9db66184c
Muhammad Hussain <k213584@nu.edu.pk>
2025-04-21 04:14:00 +0500

    Completed lab

---
+++ challenge.ts
@@ -0,0 +1,7 @@
+/**
+ * TODO:
+ * Go ahead and create a script from scratch that will allow you 
+ * to transfer SOL from one account to another on Devnet. 
+ * Be sure to print out the transaction signature so you can 
+ * look at it on Solana Explorer.
+ */
